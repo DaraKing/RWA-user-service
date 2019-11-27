07269dd4c3ccdb0be5d5190072d5d35f69ecef60
DaraKing <dario.knezovich@gmail.com>
2019-11-27 09:57:28 +0100

    cors fix v2

--- router.js
+++ router.js
@@ -3,18 +3,33 @@ let test = require('./controllers/testController');
 let cors = require('cors');
 let authController = require('./controllers/authController');
 
-let corsOptions = {
-    origin: 'http://rwafrontend-env.sw7w4bhifr.eu-central-1.elasticbeanstalk.com'
-};
-
 module.exports = function(app) {
+
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
 
     app.group("/api", (api) => {
         api.get('/test', test.test);
 
         api.group("/auth", (auth) => {
-            auth.post('/register',cors(corsOptions), authController.register);
-            auth.post('/login', cors(corsOptions), authController.login);
+            auth.post('/register', authController.register);
+            auth.post('/login', authController.login);
         })
     });
 
