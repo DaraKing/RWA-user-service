b5b00dd437be6626c267f8653ff680e4441cf010
DaraKing <dario.knezovich@gmail.com>
2019-11-27 09:59:17 +0100

    cors fix v3

--- router.js
+++ router.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, access-control-allow-origin');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
