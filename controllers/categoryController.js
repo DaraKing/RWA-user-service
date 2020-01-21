06fe85f742d8753142e75e216e59927bf122c7e6
AnteAntonini <anteantonini.mtk@hotmail.com>
2020-01-21 17:30:12 +0100

    dodo categoryController i stavio nove rute u router?,aj vidi dara jel valja ista

---
+++ controllers/categoryController.js
@@ -0,0 +1,64 @@
+let database = require("../database");
+let responses = require('../common/responses');
+let messages = require("../common/messages");
+
+module.exports = {
+    // ID param from URL you will get using req.params["category_id"]
+    getAllCategory: function (req, resp) {
+        let sql = `SELECT * FROM categories `;
+
+        database.exec(sql, (error, response) => {
+            if(error) {
+                responses.internalServerErr(req, resp, messages.DATABASE_ERROR);
+            }else {
+                responses.statusOk(req,resp, response);
+            }
+        });
+    },
+    getSingleCategory: function (req, resp) {
+        let sql = `SELECT * FROM categories WHERE category_id = '${req.params["category_id"]}'`;
+
+        database.exec(sql, (error, response) => {
+            if(error) {
+                responses.internalServerErr(req, resp, messages.DATABASE_ERROR);
+            }else {
+                responses.statusOk(req,resp, response[0]);
+            }
+        });
+    },
+    createCategory: function (req, resp) {
+        let data = {category_name: req.body.category_name, category_description: req.body.category_description};
+        let sql = `INSERT INTO categories SET?`
+        
+        database.exec(sql, data, (error, response) => {
+            if(error) {
+                responses.internalServerErr(req, resp, messages.DATABASE_ERROR);
+            }else {
+                responses.statusOk(req, resp, '{"message": "Category data has been successfully created!"}');
+            }
+        });
+    },
+    updateCategory: function (req, resp) {
+        let sql = `UPDATE categories SET category_name ='${req.body.category_name}', category_description='${req.body.category_description}'`;
+
+        database.exec(sql, (error, response) => {
+            if(error) {
+                responses.internalServerErr(req, resp, messages.DATABASE_ERROR);
+            }else {
+                responses.statusOk(req, resp, '{"message": "Category data has been successfully changed!"}');
+            }
+        })
+    },
+    deleteCategory: function (req, resp) {
+        let sql = `DELETE FROM categories WHERE category_id='${req.body.category_id}'`;
+
+        database.exec(sql, (error, response) => {
+            if(error) {
+                responses.internalServerErr(req, resp, messages.DATABASE_ERROR);
+            }else {
+                responses.statusOk(req,resp, '{"message": "Category has been deleted!"}');
+            }
+        });
+    }
+};
+
